c20af934c7327a16ffd07313535cdafc32e56a12
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-07-25 14:27:52 +0530

    .

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,4 +1,8 @@
-function startTime() {
+function checkTime(i) {
+    return (i < 10) ? "0" + i : i;
+}
+function startTime()
+{
     var today = new Date();
     var date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
     var h = today.getHours();
@@ -6,25 +10,6 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML = "Date : " + date + " " + h + ":" + m + ":" + s;
+    document.getElementById('time').innerHTML = date + " " + h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
-function checkTime(i) {
-    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-    return i;
-}
-
-var i = 0;
-function change() {
-    var doc = document.getElementById("title");
-    var color = [
-        ["#66ff66", "#ccff33"],
-        ["coral", "brown"]
-    ];
-    doc.style.color = color[i][0];
-
-    var doc = document.getElementById("titlespan");
-    doc.style.color = color[i][1];
-    i = (i + 1) % color.length;
-}
-setInterval(change, 5000);
