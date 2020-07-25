097c1c3b7ef7d286fe3cea25e2a208220c42803e
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-07-25 21:58:39 +0530

    .

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,21 +1,43 @@
+const player1 = 'X';
+const player2 = 'O';
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
+
+var current_mode = 0;
+/*
+    0 - player vs player
+    1 - player vs computer
+*/
+
 function clearBoard() {
+    var cells = document.querySelectorAll(".cell");
+    for(let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = "dhriaj";
+    }
 }
 
-var cur_mode = 0;
+
 function changeMode() {
-    var mode = document.getElementById("mode");
-    if (cur_mode == 0) {
-        mode.innerHTML = "Player vs Computer";
+    if (current_mode == 0) {
+        document.getElementById("mode").innerHTML = "Player vs Computer";
         document.getElementById("player1").innerHTML = "Player(X)";
         document.getElementById("player2").innerHTML = "Computer(O)";
     }
     else {
-        mode.innerHTML = "Player1 vs Player2";
+        document.getElementById("mode").innerHTML = "Player1 vs Player2";
         document.getElementById("player1").innerHTML = "Player1(X)";
         document.getElementById("player2").innerHTML = "Player2(O)";
     }
     clearBoard();
-    cur_mode = 1 - cur_mode;
+    current_mode = 1 - current_mode;
 }
 
 
@@ -29,10 +51,11 @@ function startTime()
     var today = new Date();
     var date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
     var h = today.getHours();
+    if(h > 12) h -= 12;
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('time').innerHTML = date + " " + h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    setTimeout(startTime, 500);
 }
