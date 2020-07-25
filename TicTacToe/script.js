3671e073fe22529deec430864947b5953868736a
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-07-25 18:35:02 +0530

    score

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,3 +1,26 @@
+function clearBoard() {
+
+}
+
+var cur_mode = 0;
+function changeMode() {
+    var mode = document.getElementById("mode");
+    if (cur_mode == 0) {
+        mode.innerHTML = "Player vs Computer";
+        document.getElementById("player1").innerHTML = "Player";
+        document.getElementById("player2").innerHTML = "Computer";
+    }
+    else {
+        mode.innerHTML = "Player1 vs Player2";
+        document.getElementById("player1").innerHTML = "Player1";
+        document.getElementById("player2").innerHTML = "Player2";
+    }
+    clearBoard();
+    cur_mode = 1 - cur_mode;
+}
+
+
+// date and time
 function checkTime(i) {
     return (i < 10) ? "0" + i : i;
 }
