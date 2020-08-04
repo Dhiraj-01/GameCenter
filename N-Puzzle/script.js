51807ec439e2c7137017f9265eca945f852273cd
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-08-04 13:34:40 +0530

    typing effect

--- N-Puzzle/script.js
+++ N-Puzzle/script.js
@@ -2,9 +2,15 @@ const minN = 3;
 const maxN = 8;
 
 function decreseN() {
-    
+    let n = (Number)(document.getElementById("n").innerHTML);
+    if(n > minN) {
+        document.getElementById("n").innerHTML = n - 1;
+    }
 }
 
 function incerseN() {
-
+    let n = (Number)(document.getElementById("n").innerHTML);
+    if (n < maxN) {
+        document.getElementById("n").innerHTML = n + 1;
+    }
 }
