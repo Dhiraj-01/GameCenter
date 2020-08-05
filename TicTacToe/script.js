22113cd83bc81818362b5c307d88bcc64a07d431
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-08-05 18:53:47 +0530

    .

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,8 +1,11 @@
-function d(p) {
-    console.log(p);
+// debug function
+function d() {
+    for(var i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+    console.log('\n');
 }
 
-// ◯ ✕ ✗
 const cross = '✕';
 const zero = '◯';
 
@@ -10,11 +13,12 @@ const player1 = cross;
 const player2 = zero;
 var cur_player = null;
 
-var game_mode = 0;
-/*
-    0 - player vs player
-    1 - player vs computer
-*/
+var pvsp = "Player1 vs Player2";
+var pvsc = "Player vs Computer";
+var game_mode = null;
+
+var board = [null, null, null, null, null, null, null, null, null];
+var cells = document.querySelectorAll(".cell");
 
 const winning_combinations = [
     [0, 1, 2],
@@ -27,107 +31,81 @@ const winning_combinations = [
     [2, 5, 8]
 ]
 
-function gameOver() {
-    for(let i = 0; i < winning_combinations.length; i++) {
-        let cell1 = winning_combinations[i][0];
-        let cell2 = winning_combinations[i][1];
-        let cell3 = winning_combinations[i][2];
+// function checkWin() {
+//     let wc = winning_combinations;
+//     for(let i = 0; i < wc.length; i++)
+//     {
+//         let cell1 = wc[i][0];
+//         let cell2 = wc[i][1];
+//         let cell3 = wc[i][2];
 
-        if (board[cell1] == null || board[cell2] == null || board[cell3] == null) {
-            continue;
-        }
-        if (board[cell1] == board[cell2] && board[cell2] == board[cell3] && board[cell3] == board[cell1]) {
-            return true;
-        }
-    }
-    return false; 
-}
+//         if (board[cell1] == null || board[cell2] == null || board[cell3] == null) {
+//             continue;
+//         }
+//         if (board[cell1] == board[cell2] && board[cell2] == board[cell3] && board[cell3] == board[cell1]) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
-function tie() {
-    for(let i = 0; i < board.length; i++) {
-        if(board[i] == null) {
-            return false;
-        }
-    }
-    return true;
-}
+// function checkTie() {
+//     for(let i = 0; i < board.length; i++) {
+//         if(board[i] == null) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-var board = [null, null, null, null, null, null, null, null, null];
+// function isEmpty(cell) {
+//     return (board[cell] == null);
+// }
 
-function isEmpty(cell) {
-    return board[cell] == null;
-}
+// function clearBoard() {
+//     for(let i = 0; i < cells.length; i++) {
+//         cells[i].innerHTML = null;
+//         board[i] = null;
+//     }
+//     cur_player = player1;
+// }
 
-function clearBoard() {
-    var cells = document.querySelectorAll(".cell");
-    for(let i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = null;
-    }
-    for(let i = 0; i < board.length; i++) {
-        board[i] = null;
-    }
-    cur_player = player1;
-}
+// function changePlayer() {
+//     cur_player = (cur_player == player1) ? player2 : player1;
+// }
 
-function changePlayer() {
-    if(cur_player == player1) {
-        cur_player = player2;
-    }
-    else {
-        cur_player = player1;
-    }
-}
+// function press(e) {
+//     let cell = e.target.id;
+//     if(gameOver() || tie()) {
+//         clearBoard();
+//         return;
+//     }
 
-function press(e) {
-    let cell = e.target.id;
-    if(gameOver() || tie()) {
-        clearBoard();
-        return;
-    }
+//     if(isEmpty(cell))
+//     {
+//         let sound = document.getElementById("sound");
+//         sound.play();
 
-    if(isEmpty(cell))
-    {
-        let sound = document.getElementById("sound");
-        sound.play();
+//         board[cell] = cur_player;
+//         document.getElementById(cell).innerHTML = cur_player;
+//         if(gameOver()) {
+//             // clearBoard();
+//         }
+//         else changePlayer();
+//     }
+// }
 
-        board[cell] = cur_player;
-        document.getElementById(cell).innerHTML = cur_player;
-        if(gameOver()) {
-            // clearBoard();
-        }
-        else changePlayer();
-    }
-}
-
-function changeMode() {
-    if (game_mode == 0) {
-        document.getElementById("mode").innerHTML = "Player vs Computer";
-        document.getElementById("player1").innerHTML = "Player(X)";
-        document.getElementById("player2").innerHTML = "Computer(O)";
-    }
-    else {
-        document.getElementById("mode").innerHTML = "Player1 vs Player2";
-        document.getElementById("player1").innerHTML = "Player1(X)";
-        document.getElementById("player2").innerHTML = "Player2(O)";
-    }
-    clearBoard();
-    game_mode ^= 1;
-}
-
-
-// date and time
-function checkTime(i) {
-    return (i < 10) ? "0" + i : i;
-}
-
-function startTime() {
-    var today = new Date();
-    var date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML = date + " " + h + ":" + m + ":" + s;
-    setTimeout(startTime, 500);
-}
+// function changeMode() {
+//     if (game_mode == 0) {
+//         document.getElementById("mode").innerHTML = "Player vs Computer";
+//         document.getElementById("player1").innerHTML = "Player(X)";
+//         document.getElementById("player2").innerHTML = "Computer(O)";
+//     }
+//     else {
+//         document.getElementById("mode").innerHTML = "Player1 vs Player2";
+//         document.getElementById("player1").innerHTML = "Player1(X)";
+//         document.getElementById("player2").innerHTML = "Player2(O)";
+//     }
+//     clearBoard();
+//     game_mode ^= 1;
+// }
